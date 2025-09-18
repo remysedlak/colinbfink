9dcd4fe3d0d515ed633974e90d56cd78f6883fcc
remysedlak <remysedlak@gmail.com>
2025-09-18 17:39:59 -0400

    update aboutme to use real text instead of lorem ipsum

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -4,15 +4,7 @@ function AboutMe() {
       <h1 className="text-4xl italic font-bold text-center">About Me</h1>
       <div className="mt-4 w-80 md:w-120 lg:180  bg-transparent border-y-2 p-6">
         <h1 className="text-3xl font-bold">Hi I am Colin Fink</h1>
-        <p className="text-xl">
-          lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+        <p className="text-xl">Hi, I’m Colin Beckham Fink, a filmmaker from Pittsburgh, Pennsylvania. I’m the Director, Writer, Editor, and CEO of Good One Media, an independent production company I founded. Currently, I’m pursuing a degree in Film & Media Studies at the University of Pittsburgh, where I’m actively involved with UPTV, the student-run television station, and collaborate with a growing network of independent filmmakers.</p>
       </div>
     </div>
   );
